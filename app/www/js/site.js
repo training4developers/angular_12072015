955afb27c04cf17337ef521628918021fe1b636c
Eric W. Greene <eric@ericwgreene.com>
2015-12-11 11:15:02 -0700

    add ui router code demos

--- app/www/js/site.js
+++ app/www/js/site.js
@@ -33248,4 +33248,5 @@ is set to `true`. The parse error is stored in `ngModel.$error.parse`.
 
 angular.module("MyApp.Templates", []).run([ "$templateCache", function($templateCache) {
   "use strict";
+  $templateCache.put("home.tpl.html", '<table class="table table-striped"><thead><tr><th>Name</th><th>Color</th><th>Size</th><th>Quantity</th><th>Action</th></tr></thead><tbody><tr ng-repeat="widget in widgets"><td>{{widget.name}}</td><td>{{widget.color}}</td><td>{{widget.size}}</td><td>{{widget.quantity}}</td><td><button class=btn>Edit</button> <button class=btn>View</button></td></tr></tbody></table>');
 } ]);
